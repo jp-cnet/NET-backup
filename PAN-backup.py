1fdea911330873526ba1ed43cf887fc2d382927c
JP <jpierson@compunet.biz>
2019-01-28 15:14:32 -0700

    Update to path builder

--- PAN-backup.py
+++ PAN-backup.py
@@ -12,12 +12,17 @@ key = "YOUR API KEY GOES HERE"
 #build URI with previously provided IP and key
 uri = "https://"+fwip+"/api/?type=export&category=configuration&key="+key
 
+mypath = "/home/jeremy/backup/pa"
+filename = "sobitnet-pa"
+
 #filename to write
-myfile = datetime.datetime.now().strftime("%y-%m-%d-%H-%M")+"-jpbackup.xml"
+myfile = mypath+"/"+datetime.datetime.now().strftime("%y-%m-%d-%H-%M")+"-"+filename+".xml"
 
 #output to verify requested resource
 print("Requesting the following resource:")
 print(uri)
+print("File to be written:")
+print(myfile)
 
 #http request config from management IP and API key provided
 r = requests.get(uri, verify=False)
